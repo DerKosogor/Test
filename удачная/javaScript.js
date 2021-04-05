aa2b6c66ff7f58ce8db32ceeb605ae9f87c33e0d
DerKosogor <13as90@mail.ru>
2021-04-05 15:18:45 +0300

    remove der удачная

--- удачная/javaScript.js
+++
@@ -1,7 +0,0 @@
-let page = document.querySelector('.page');
-let themeButton = document.querySelector('.theme-button');
-
-themeButton.onclick = function() {
-    page.classList.toggle('theme1');
-    page.classList.toggle('theme2');
-};
