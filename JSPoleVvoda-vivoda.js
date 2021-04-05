7dac7626d522a7b8a753314f2395cf68514a317f
DerKosogor <13as90@mail.ru>
2021-04-05 15:31:20 +0300

    new

---
+++ JSPoleVvoda-vivoda.js
@@ -0,0 +1,5 @@
+function getDataForm() {
+    let conclusion = document.querySelector('.conclusion');
+    let input = document.querySelector('.inputData');
+    conclusion.textContent = input.value;
+}
---
+++ likes.js
@@ -0,0 +1,23 @@
+//обьявляем переменную для кнопки сердечко
+let heart = document.querySelector('.heart');
+//обьявляем переменную для вложенного в срдечко тега span, оно отображает число лайков
+let likesNumber = document.querySelector('.likes-number');
+//обявляем числовое значение переменной( она есть только в js)
+
+
+
+heart.onclick = function () {
+    //переменная counter++; особая, она добавляет 1 (что бы добавить 2 или более надо писать так counter += 2;)
+    //ниже -если у кнопки сердечко есть класс added то выполнить условие -убрать у span единицу
+    if (heart.classList.contains('added') ) {
+        likesNumber.textContent--;
+    }
+    //если класса added нет то выполнить +1
+    else {
+        likesNumber.textContent++;
+    }
+
+    heart.classList.toggle('added');
+};
+
+
