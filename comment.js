1b69ce4a231a6cf7eab9f11d86d34ec1fb0ae6f2
DerKosogor <13as90@mail.ru>
2021-04-05 18:37:45 +0300

    add comment

---
+++ comment.js
@@ -0,0 +1,19 @@
+//при возникновении события getDataFormComment (onclick) запускается обработчик событий
+function getDataFormComment() {
+//обьявляем переменную для всего списка коментариев
+let commentList = document.querySelector('.comment-list');
+//обьявляем переменную для поля ввода коментариев
+let commentField = document.querySelector('.comment-field');
+
+
+//обявляем переменную для создать новый элемент пункт списка
+    let newComment = document.createElement('li');
+    //давить класс юзеркоммент новому пункту списка
+    newComment.classList.add('user-comment');
+    //занести текст из поля ввода в новый пункт списка
+    newComment.textContent = commentField.value;
+    //очистить поел ввода
+    commentField.value = '';
+    //добавляем новое содержимое к старому а именно, в список коментариев добавляем новый пункт с текстом
+    commentList.append(newComment);
+};
