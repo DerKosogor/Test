25d7a04deac7d22b15a4f3f70faec8a89a69554b
DerKosogor <13as90@mail.ru>
2021-04-10 16:36:56 +0300

    button check

--- comment.js
+++ comment.js
@@ -1,9 +1,14 @@
-//при возникновении события getDataFormComment (onclick) запускается обработчик событий
-function getDataFormComment() {
 //обьявляем переменную для всего списка коментариев
 let commentList = document.querySelector('.comment-list');
 //обьявляем переменную для поля ввода коментариев
 let commentField = document.querySelector('.comment-field');
+// обьявляем переменную для спан поля , где будет отображаться счетчик символов
+let charCounter = document.querySelector('.char-counter');
+// обьявляем переменную для кнпки отправки коментария
+let submitButton = document.querySelector('.buttoncom');
+
+//при возникновении события getDataFormComment (onclick) запускается обработчик событий
+function getDataFormComment() {
 
 
 //обявляем переменную для создать новый элемент пункт списка
@@ -16,4 +21,22 @@ let commentField = document.querySelector('.comment-field');
     commentField.value = '';
     //добавляем новое содержимое к старому а именно, в список коментариев добавляем новый пункт с текстом
     commentList.append(newComment);
+    //обнуляем значение использованых символов в поле ввода, что бы после отправке сообщения счетчик показывал 0
+    charCounter.value = '0';
+};
+
+
+
+    commentField.oninput = function  () {
+        //при каждом новом вводе символа публикует их количество в спец блок с помощью length
+    charCounter.textContent = commentField.value.length;
+// условие: если длина текста больше 15 символовв то
+        if (commentField.value.length > 15) {
+  // то заблокировать кнопку ввода
+            submitButton.disabled = true;
+        }
+ // иначе: разблокировать кнопку ввода
+        else {
+            submitButton.disabled = false;
+        }
 };
--- explanation.js
+++ explanation.js
@@ -3,18 +3,16 @@ let closeButton = document.querySelector('.close-button');
 let tooltipButtons = document.querySelectorAll('.tooltip-button');
 let tooltipText = document.querySelector('.tooltip-text');
 
+//задаем цикл через фо для всех и каждого элемента тултип батонс. Он будет повторятся для всех кнопок сколько бы их ни было
+for (let tooltipButton of tooltipButtons) {
+    tooltipButton.onclick = function () {
+        // присовить текст классу tooltip text
+        tooltipText.textContent = tooltipButton.dataset.tooltipText;
+        // добавляется класс опенет тултипу
+        tooltip.classList.add('opened');
+    };
+}
 
-tooltipButtons[0].onclick = function () {
-    // присовить текст классу tooltip text
-    tooltipText.textContent = tooltipButtons[0].dataset.tooltipText;
-    // добавляется класс опенет тултипу
-    tooltip.classList.add('opened');
-};
-
-tooltipButtons[1].onclick = function () {
-    tooltipText.textContent = tooltipButtons[1].dataset.tooltipText;
-    tooltip.classList.add('opened');
-};
 
 closeButton.onclick = function () {
     tooltip.classList.remove('opened');
